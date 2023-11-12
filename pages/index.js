baaa93489c247bfbbe4cd3ed3456353c650f6245
Merve Balci <mervebalci090@gmail.com>
2023-11-12 14:21:34 -0400

    Polishing the Index Page

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
+import Date from '../components/date';
 import { getSortedPostsData } from '../lib/posts'
 
 export async function getStaticProps() {
@@ -21,8 +22,7 @@ export default function Home({ allPostsData }) {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>
-          Hello, I'm Merve. I'm a chemical engineer and learning software development. You can find more about me {' '}
-          <Link href="/posts/first-post">here!</Link>
+          Hello, I'm Merve. I'm a chemical engineer and learning software development.
         </p>
         <p>
           (This is a sample website - I'll be building a site like this on {' '}
@@ -34,12 +34,12 @@ export default function Home({ allPostsData }) {
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
-            <li>
-              {title}
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
